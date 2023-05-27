a4d1bcfcc207450b7e9ac2b743d448126f045085
Eduardo <eduardo.acosta@imagemaker.com>
2023-05-27 12:32:09 -0500

    update ico

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -21,7 +21,7 @@ const Meta = () => {
         sizes="16x16"
         href="/icons/favicon-16x16.png"
       />
-      <link rel="icon" type="image/png" href="icons/favicon.ico" sizes="32x32" />
+      <link rel="icon" href="/favicon.ico" />
       <meta
         name="description"
         content={`${CMS_NAME}.`}
