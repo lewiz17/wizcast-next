d5c2e434f2fa10fef4e4838f11c11d94811b13b4
Eduardo <eduardo.acosta@imagemaker.com>
2023-09-03 11:22:20 -0500

    update anime

--- pages/series/index.tsx
+++ pages/series/index.tsx
@@ -39,6 +39,7 @@ type Props = {
     netflix: Serie[];
     disney: Serie[];
     prime: Serie[];
+    anime: Serie[];
   };
 };
 
@@ -50,6 +51,13 @@ export default function ListItems({ series }: Props) {
           <title>{`${CMS_NAME} - Estrenos ${new Date().getFullYear()}`}</title>
         </Head>
         <Container>
+          {series.anime.length > 0 ? (
+            <SliderBox series={series.anime} title={"Anime"} />
+          ) : (
+            <p className="flex justify-center items-center text-2xl h-[200px]">
+              Cargando...
+            </p>
+          )}
           {series.top.length > 0 ? (
             <SliderBox series={series.top} title={"Top Series"} />
           ) : (
@@ -86,7 +94,7 @@ export default function ListItems({ series }: Props) {
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
   const currentDate = new Date().toISOString().split("T")[0];
-  const [nowPlayingSeries, netflixSeries, disneySeries, primeSeries] =
+  const [nowPlayingSeries, netflixSeries, disneySeries, primeSeries, anime] =
     await Promise.all([
       fetch(
         `https://api.themoviedb.org/3/trending/tv/day?api_key=a0a7e40dc8162ed7e37aa2fc97db5654&language=es-MX`
@@ -100,12 +108,16 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
       fetch(
         `https://api.themoviedb.org/3/discover/tv?api_key=a0a7e40dc8162ed7e37aa2fc97db5654&language=es-MX&with_watch_providers=119&with_runtime.lte=400&sort_by=popularity.desc&watch_region=CO`
       ),
+      fetch(
+        `https://api.themoviedb.org/3/discover/tv?api_key=a0a7e40dc8162ed7e37aa2fc97db5654&language=es-MX&with_watch_providers=283&with_runtime.lte=400&sort_by=popularity.desc&watch_region=CO`
+      ),
     ]);
 
   const nowPlayingData = await nowPlayingSeries.json();
   const netflixData = await netflixSeries.json();
   const disneyData = await disneySeries.json();
   const primeData = await primeSeries.json();
+  const animeData = await anime.json();
 
   const nowPlayingSeriesData = nowPlayingData.results.map((serie) => ({
     id: serie.id,
@@ -139,10 +151,19 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
     rate: serie.vote_average,
   }));
 
-  const nowSeriesFinal = nowPlayingSeriesData.filter((v) => v.rate >= 7.5);
-  const netflixSeriesFinal = netflixSeriesData.filter((v) => v.rate >= 7.5);
-  const disneySeriesFinal = disneySeriesData.filter((v) => v.rate >= 7.5);
-  const primeSeriesFinal = primeSeriesData.filter((v) => v.rate >= 7.5);
+  const animeSeriesData = animeData.results.map((serie) => ({
+    id: serie.id,
+    title: serie.name,
+    poster: serie.poster_path,
+    release: serie.first_air_date,
+    rate: serie.vote_average,
+  }));
+
+  const nowSeriesFinal = nowPlayingSeriesData.filter((v) => v.rate >= 7);
+  const netflixSeriesFinal = netflixSeriesData.filter((v) => v.rate >= 7);
+  const disneySeriesFinal = disneySeriesData.filter((v) => v.rate >= 7);
+  const primeSeriesFinal = primeSeriesData.filter((v) => v.rate >= 7);
+  const animeSeriesFinal = animeSeriesData.filter((v) => v.rate >= 7);
 
   return {
     props: {
@@ -151,6 +172,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async () => {
         netflix: [...netflixSeriesFinal],
         disney: [...disneySeriesFinal],
         prime: [...primeSeriesFinal],
+        anime: [...animeSeriesFinal],
       },
     },
   };
