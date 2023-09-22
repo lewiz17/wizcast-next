149c42c543a3e623df2b37845fe38fcb7ebde4c9
Eduardo <eduardo.acosta@imagemaker.com>
2023-09-22 14:14:42 -0500

    update contact form

--- components/FormContact.tsx
+++ components/FormContact.tsx
@@ -1,107 +1,18 @@
 import { useState } from "react";
 
 export default function ContactForm() {
-  /*
-  const [formValues, setFormValues] = useState({
-    mail: "",
-    subject: "",
-    message: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-  };
-
-  const encode = (data) => {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  };
-
-  const handleSubmit = (e) => {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contacto", ...formValues }),
-    })
-      .then(() => alert("Mensaje enviado!"))
-      .catch((error) => alert(error));
-
-    e.preventDefault();
-  };
-*/
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-black dark:text-black">
-          Escribenos...
-        </h2>
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          className="space-y-8"
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSfU7-An3s9DNgW1HGfL61kYxu9D3cJD5Qv858KnHIJ29huyhw/viewform?embedded=true"
+          width="500"
+          height="395"
+          marginHeight={0}
+          marginWidth={0}
         >
-          <div>
-            <label
-              htmlFor="email"
-              className="block mb-2 text-sm font-medium text-black dark:text-gray-300"
-            >
-              Correo Electronico
-            </label>
-            <input
-              type="email"
-              id="email"
-              className="shadow-sm bg-[#021420e0] border border-gray text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              placeholder="nombre@gmail.com"
-              required
-              name="correo"
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="subject"
-              className="block mb-2 text-sm font-medium text-black dark:text-gray-300"
-            >
-              Asunto
-            </label>
-            <input
-              type="text"
-              id="subject"
-              name="asunto"
-              className="shadow-sm bg-[#021420e0] border border-gray text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              placeholder="Describenos lo encontrado"
-              required
-            />
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block mb-2 text-sm font-medium text-black dark:text-gray-400"
-            >
-              Detalles del reporte
-            </label>
-            <textarea
-              id="message"
-              rows={6}
-              name="mensaje"
-              className="block p-2.5 w-full text-sm text-white bg-[#021420e0] rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Detalles de la falla (opcional)"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-black sm:w-fit hover:bg-gray focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >
-            Enviar reporte
-          </button>
-        </form>
+          Cargando…
+        </iframe>
       </div>
     </section>
   );
